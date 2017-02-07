e79b0e372bccab4bd31b7706224a02961767e218
washburns1021 <washburns1021@gmail.com>
2017-02-06 22:07:30 -0500

    room

--- CS_Chapter2/Room/Program.cs
+++ CS_Chapter2/Room/Program.cs
@@ -9,6 +9,11 @@ namespace Room
     {
         static void Main(string[] args)
         {
+            int width = 10;
+            int length = 20;                      
+            int area = length * width;
+
+            Console.WriteLine("The floor space is " + area + " sqaure feet "); 
         }
-    }
-}
+    }   
+}   
